b31ec6626aff5261d9843e1a0b80615ff7a57f6d
dk1a <dk1a@protonmail.com>
2023-05-24 19:57:13 +0300

    fix spaces

--- packages/contracts/test/LibExperience.t.sol
+++ packages/contracts/test/LibExperience.t.sol
@@ -28,6 +28,7 @@ contract LibExperienceTest is MudV2Test {
       assertEq(exp[i], 1);
     }
   }
+
   function testGetExp() public {
     LibExperience.initExp(targetEntity);
     assertTrue(LibExperience.hasExp(targetEntity));
@@ -39,54 +40,53 @@ contract LibExperienceTest is MudV2Test {
     for (uint256 i; i < exp.length; i++) {
       assertEq(exp[i], 1);
     }
-}
+  }
 
   function testHasExp() public {
     assertFalse(LibExperience.hasExp(targetEntity));
 
     LibExperience.initExp(targetEntity);
     assertTrue(LibExperience.hasExp(targetEntity));
-}
-
-function testSetExp() public {
-  uint32[PStat_length] memory exp = [uint32(10), 20, 30];
-  Experience.set(targetEntity, exp);
-
-  uint32[PStat_length] memory retrievedExp = LibExperience.getExp(targetEntity);
-  for (uint256 i; i < retrievedExp.length; i++) {
-    assertEq(retrievedExp[i], exp[i]);
-  }
-}
-
-function testAddExp() public {
-  LibExperience.initExp(targetEntity);
-  assertTrue(LibExperience.hasExp(targetEntity));
-
-  uint32[PStat_length] memory initialExp = LibExperience.getExp(targetEntity);
-
-  uint32[PStat_length] memory addExp = [uint32(5), 10, 15];
-  LibExperience.increaseExp(targetEntity, addExp);
-
-  uint32[PStat_length] memory expectedExp;
-  for (uint256 i; i < initialExp.length; i++) {
-    expectedExp[i] = initialExp[i] + addExp[i];
   }
 
-  uint32[PStat_length] memory retrievedExp = LibExperience.getExp(targetEntity);
-  for (uint256 i; i < retrievedExp.length; i++) {
-    assertEq(retrievedExp[i], expectedExp[i]);
+  function testSetExp() public {
+    uint32[PStat_length] memory exp = [uint32(10), 20, 30];
+    Experience.set(targetEntity, exp);
+
+    uint32[PStat_length] memory retrievedExp = LibExperience.getExp(targetEntity);
+    for (uint256 i; i < retrievedExp.length; i++) {
+      assertEq(retrievedExp[i], exp[i]);
+    }
   }
-}
 
-function testIsMaxLevel() public {
-  LibExperience.initExp(targetEntity);
-  // assertFalse(LibExperience.isMaxLevel(targetEntity));
+  function testAddExp() public {
+    LibExperience.initExp(targetEntity);
+    assertTrue(LibExperience.hasExp(targetEntity));
 
-  // Set the experience to the maximum level
-  uint32[PStat_length] memory maxExp = [uint32(type(uint8).max), type(uint8).max, type(uint8).max];
-  Experience.set(targetEntity, maxExp);
+    uint32[PStat_length] memory initialExp = LibExperience.getExp(targetEntity);
 
-  assertLe(LibExperience.getAggregateLevel(targetEntity, maxExp), 16);
-}
+    uint32[PStat_length] memory addExp = [uint32(5), 10, 15];
+    LibExperience.increaseExp(targetEntity, addExp);
 
+    uint32[PStat_length] memory expectedExp;
+    for (uint256 i; i < initialExp.length; i++) {
+      expectedExp[i] = initialExp[i] + addExp[i];
+    }
+
+    uint32[PStat_length] memory retrievedExp = LibExperience.getExp(targetEntity);
+    for (uint256 i; i < retrievedExp.length; i++) {
+      assertEq(retrievedExp[i], expectedExp[i]);
+    }
+  }
+
+  function testIsMaxLevel() public {
+    LibExperience.initExp(targetEntity);
+    // assertFalse(LibExperience.isMaxLevel(targetEntity));
+
+    // Set the experience to the maximum level
+    uint32[PStat_length] memory maxExp = [uint32(type(uint8).max), type(uint8).max, type(uint8).max];
+    Experience.set(targetEntity, maxExp);
+
+    assertLe(LibExperience.getAggregateLevel(targetEntity, maxExp), 16);
+  }
 }
