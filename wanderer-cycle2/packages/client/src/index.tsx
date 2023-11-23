65007a4b84ca86046840b02bdc6175d96bd81cd4
R-Morpheus <gagach16rus@gmail.com>
2023-11-23 18:41:15 +0300

    move the folder

--- wanderer-cycle2/packages/client/src/index.tsx
+++ wanderer-cycle2/packages/client/src/index.tsx
@@ -3,7 +3,7 @@ import { App } from "./app/App";
 import { setup } from "./app/mud/setup";
 import { MUDProvider } from "./app/providers/MudProvider/MUDContext";
 import mudConfig from "contracts/mud.config";
-import {ErrorBoundary} from "@/app/ErrorBoundary";
+import {ErrorBoundary} from "@/app/providers/ErrorBoundary";
 
 const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
