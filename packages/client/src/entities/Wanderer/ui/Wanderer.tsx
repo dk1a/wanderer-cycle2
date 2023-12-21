fd240fd516df2af5ad6e791bb8f10e27c7086459
R-Morpheus <gagach16rus@gmail.com>
2023-12-21 16:31:25 +0300

    add: gitignore, wanderer and styles

--- packages/client/src/entities/Wanderer/ui/Wanderer.tsx
+++ packages/client/src/entities/Wanderer/ui/Wanderer.tsx
@@ -1,5 +1,6 @@
 import {classNames} from "@/shared/lib/classNames/classNames";
 import cls from './Wanderer.module.scss'
+import {Button} from "@/shared/ui/Button/Button";
 
 interface WandererProps{
   className?: string;
@@ -8,7 +9,16 @@ interface WandererProps{
 const Wanderer = ({className}: WandererProps) => {
   return (
     <div className={classNames(cls.Wanderer , {}, [className])}>
-
+        //Todo image
+      {/*<WandererImage entity={wandererEntity} />*/}
+      <div className={cls.btn}>
+        <Button disabled={true}>
+          Selected
+        </Button>
+        <Button>
+          Select
+        </Button>
+      </div>
     </div>
   );
 };
