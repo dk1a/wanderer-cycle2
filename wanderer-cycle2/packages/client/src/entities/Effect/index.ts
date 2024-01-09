eba58f8d06e9cfe9cf2cb916cf1af5b3f29d1bcd
R-Morpheus <gagach16rus@gmail.com>
2024-01-09 20:30:02 +0300

    add: effect

---
+++ wanderer-cycle2/packages/client/src/entities/Effect/index.ts
@@ -0,0 +1,6 @@
+import Effect from "./ui/effectIndex/Effect";
+import EffectNameItem from "./ui/Item/EffectNameItem";
+import EffectNameSkill from "./ui/skill/EffectNameSkill";
+import { EffectStatmod } from "./ui/statmod/EffectStatmod";
+
+export { Effect, EffectNameItem, EffectNameSkill, EffectStatmod }
