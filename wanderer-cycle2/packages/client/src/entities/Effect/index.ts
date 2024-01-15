46705038f1947da910ed3d2282642fe328c79397
R-Morpheus <107617521+R-Morpheus@users.noreply.github.com>
2024-01-15 23:05:37 +0300

    Delete extra dir

--- wanderer-cycle2/packages/client/src/entities/Effect/index.ts
+++
@@ -1,6 +0,0 @@
-import Effect from "./ui/effectIndex/Effect";
-import EffectNameItem from "./ui/Item/EffectNameItem";
-import EffectNameSkill from "./ui/skill/EffectNameSkill";
-import { EffectStatmod } from "./ui/statmod/EffectStatmod";
-
-export { Effect, EffectNameItem, EffectNameSkill, EffectStatmod }
