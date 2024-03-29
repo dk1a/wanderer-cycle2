d76bfe37c13c4e75e66164f7b7d7aa8b15537e65
R-Morpheus <gagach16rus@gmail.com>
2024-03-29 09:00:09 +0300

    LearnCycleSkillSystem

--- packages/contracts/src/cycle/LearnCycleSkillSystem.sol
+++ packages/contracts/src/cycle/LearnCycleSkillSystem.sol
@@ -1,59 +1,49 @@
 // SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
 
-pragma solidity ^0.8.17;
+import { System } from "@latticexyz/world/src/System.sol";
 
-import { System } from "solecs/System.sol";
-import { IWorld } from "solecs/interfaces/IWorld.sol";
-import { getAddressById } from "solecs/utils.sol";
-
-import { ActiveGuiseComponent, ID as ActiveGuiseComponentID } from "../guise/ActiveGuiseComponent.sol";
-import { GuiseSkillsComponent, ID as GuiseSkillsComponentID } from "../guise/GuiseSkillsComponent.sol";
-import { SkillPrototypeComponent, ID as SkillPrototypeComponentID } from "../skill/SkillPrototypeComponent.sol";
+import { ActiveGuise, SkillTemplate, SkillTemplateData, GuiseSkill } from "../codegen/index.sol";
 
 import { LibLearnedSkills } from "../skill/LibLearnedSkills.sol";
 import { LibCycle } from "./LibCycle.sol";
 import { LibGuiseLevel } from "../guise/LibGuiseLevel.sol";
 
-uint256 constant ID = uint256(keccak256("system.LearnCycleSkill"));
-
 /// @title Learn a Skill from the current cycle Guise's set of available skills.
 contract LearnCycleSkillSystem is System {
-  using LibLearnedSkills for LibLearnedSkills.Self;
-
   error LearnCycleSkillSystem__SkillNotInGuiseSkills();
   error LearnCycleSkillSystem__LevelIsTooLow();
 
-  constructor(IWorld _world, address _components) System(_world, _components) {}
-
-  function executeTyped(uint256 wandererEntity, uint256 skillEntity) public {
-    execute(abi.encode(wandererEntity, skillEntity));
-  }
-
-  function execute(bytes memory args) public override returns (bytes memory) {
-    (uint256 wandererEntity, uint256 skillEntity) = abi.decode(args, (uint256, uint256));
-
-    ActiveGuiseComponent activeGuiseComp = ActiveGuiseComponent(getAddressById(components, ActiveGuiseComponentID));
-    SkillPrototypeComponent skillProto = SkillPrototypeComponent(getAddressById(components, SkillPrototypeComponentID));
-    GuiseSkillsComponent guiseSkillsComp = GuiseSkillsComponent(getAddressById(components, GuiseSkillsComponentID));
+  function learnFromCycle(bytes memory args) public override returns (bytes memory) {
+    (bytes32 wandererEntity, bytes32 skillEntity) = abi.decode(args, (bytes32, bytes32));
 
     // get cycle entity if sender is allowed to use it
-    uint256 cycleEntity = LibCycle.getCycleEntityPermissioned(components, wandererEntity);
+    bytes32 cycleEntity = LibCycle.getCycleEntityPermissioned(wandererEntity);
 
     // check skill's level requirements
-    uint32 currentLevel = LibGuiseLevel.getAggregateLevel(components, cycleEntity);
-    uint8 requiredLevel = skillProto.getValue(skillEntity).requiredLevel;
+    uint32 currentLevel = LibGuiseLevel.getAggregateLevel(cycleEntity);
+    uint8 requiredLevel = SkillTemplate.getRequiredLevel(skillEntity);
     if (currentLevel < requiredLevel) {
       revert LearnCycleSkillSystem__LevelIsTooLow();
     }
 
     // guise skills must include `skillEntity`
-    uint256 guiseProtoEntity = activeGuiseComp.getValue(cycleEntity);
-    if (!guiseSkillsComp.hasItem(guiseProtoEntity, skillEntity)) {
-      revert LearnCycleSkillSystem__SkillNotInGuiseSkills();
+    bytes32 guiseProtoEntity = ActiveGuise.get(cycleEntity);
+    bytes32[] skills = GuiseSkill.get(guiseProtoEntity);
+
+    bool res = false;
+    for (uint i = 0; i < skills.length; i++) {
+      if (skills[i] == skillEntity) {
+        res = true;
+        break;
+      }
     }
 
+    if (!res) {
+      revert LearnCycleSkillSystem__SkillNotInGuiseSkills();
+    }
     // learn the skill
-    LibLearnedSkills.__construct(world, cycleEntity).learnSkill(skillEntity);
+    LibLearnedSkills.learnSkill(wandererEntity, skillEntity);
 
     return "";
   }
