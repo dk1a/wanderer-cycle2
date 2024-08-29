8b004ab8e4250361780349b474b8a58c704cd5f0
dk1a <dk1a@protonmail.com>
2024-08-29 18:09:43 +0300

    add mint, burn

--- packages/contracts/src/token/LibERC721.sol
+++ packages/contracts/src/token/LibERC721.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
+import { IERC721Mintable } from "@latticexyz/world-modules/src/modules/erc721-puppet/IERC721Mintable.sol";
+import { _tokenUriTableId, _ownersTableId } from "@latticexyz/world-modules/src/modules/erc721-puppet/utils.sol";
 import { Owners } from "@latticexyz/world-modules/src/modules/erc721-puppet/tables/Owners.sol";
 import { TokenURI } from "@latticexyz/world-modules/src/modules/erc721-puppet/tables/TokenURI.sol";
 
-import { _tokenUriTableId } from "@latticexyz/world-modules/src/modules/erc721-puppet/utils.sol";
-import { _ownersTableId } from "@latticexyz/world-modules/src/modules/erc721-puppet/utils.sol";
+import { getUniqueEntity } from "@latticexyz/world-modules/src/modules/uniqueentity/getUniqueEntity.sol";
 
 import { ERC721Config } from "../codegen/index.sol";
 
@@ -22,6 +23,25 @@ library LibERC721 {
 
   function tokenAddress(ERC721Namespace namespace) internal view returns (address) {
     return ERC721Config.get(namespace.unwrap());
+  }
+
+  function mint(ERC721Namespace namespace, address to) internal returns (bytes32 tokenEntity) {
+    // Token id is also a globally unique entity
+    tokenEntity = getUniqueEntity();
+    uint256 tokenId = uint256(tokenEntity);
+
+    IERC721Mintable erc721 = IERC721Mintable(namespace.tokenAddress());
+    erc721.mint(to, tokenId);
+
+    return tokenEntity;
+  }
+
+  function burn(ERC721Namespace namespace, bytes32 tokenEntity) internal {
+    // Token id is also a globally unique entity
+    uint256 tokenId = uint256(tokenEntity);
+
+    IERC721Mintable erc721 = IERC721Mintable(namespace.tokenAddress());
+    erc721.burn(tokenId);
   }
 
   function tokenURI(ERC721Namespace namespace, uint256 tokenId) internal view returns (string memory) {
