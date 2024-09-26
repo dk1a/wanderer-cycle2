ab92b0231aa060d3afd7e491ea9f8ff1236a2baa
R-Morpheus <gagach16rus@gmail.com>
2024-09-26 23:45:38 +0300

    skills

--- packages/client/src/components/Guise/GuiseSkill.tsx
+++ packages/client/src/components/Guise/GuiseSkill.tsx
@@ -1,10 +1,16 @@
 import { Tooltip } from "react-tooltip";
+import Skill from "./Skill";
+import { useSkillStrict } from "../../mud/hooks/skill";
+import { Entity } from "@latticexyz/recs";
 
 interface GuiseSkillProps {
   className?: string;
+  entity: Entity;
 }
 
-export const GuiseSkill = ({ className }: GuiseSkillProps) => {
+export const GuiseSkill = ({ className, entity }: GuiseSkillProps) => {
+  const skill = useSkillStrict(entity);
+
   return (
     <div className={"bg-dark-500 p-2 border border-dark-400 flex " + className}>
       <div className={"w-full flex cursor-pointer justify-between"}>
@@ -14,15 +20,15 @@ export const GuiseSkill = ({ className }: GuiseSkillProps) => {
               "text-dark-number text-lg cursor-pointer w-6 text-center mr-0.5"
             }
           >
-            {"skill.requiredLevel"}.
+            {skill.requiredLevel}.
           </div>
           <div className={"text-dark-method text-lg cursor-pointer w-full"}>
-            {"skill.name"}
+            {skill.name}
           </div>
         </div>
       </div>
-      <Tooltip anchorSelect={"."} place={"top"} className={}>
-        {/*<Skill />*/}
+      <Tooltip anchorSelect={"."} place={"top"} className={""}>
+        <Skill skill={skill} />
       </Tooltip>
     </div>
   );
---
+++ packages/client/src/mud/hooks/skill.ts
@@ -0,0 +1,105 @@
+import { useComponentValue, useEntityQuery } from "@latticexyz/react";
+import { Entity, Has } from "@latticexyz/recs";
+import { useCallback, useMemo } from "react";
+import { getSkill } from "../utils/skill";
+import { useMUD } from "../../MUDContext";
+
+export const useSkill = (entity: Entity | undefined) => {
+  const { world, components } = useMUD();
+
+  return useMemo(() => {
+    if (entity === undefined) return undefined;
+    return getSkill(world, components, entity);
+  }, [world, components, entity]);
+};
+
+export const useSkillStrict = (entity: Entity) => {
+  const { world, components } = useMUD();
+
+  return useMemo(() => {
+    return getSkill(world, components, entity);
+  }, [world, components, entity]);
+};
+
+export const useSkills = (entities: Entity[]) => {
+  const { world, components } = useMUD();
+
+  return useMemo(() => {
+    return entities.map((entity) => {
+      return getSkill(world, components, entity);
+    });
+  }, [world, components, entities]);
+};
+
+export const useAllSkillEntities = () => {
+  const {
+    components: { SkillPrototype },
+  } = useMUD();
+
+  return useEntityQuery([Has(SkillPrototype)]);
+};
+
+export const useLearnedSkillEntities = (targetEntity: Entity | undefined) => {
+  const {
+    world,
+    components: { LearnedSkills },
+  } = useMUD();
+
+  const learnedSkills = useComponentValue(LearnedSkills, targetEntity);
+  return useMemo(() => {
+    const entityIds = learnedSkills?.value ?? [];
+    return entityIds.map((entityId) => {
+      const entity = world.entityToIndex.get(entityId);
+      if (entity === undefined)
+        throw new Error(`No index for entity id ${entityId}`);
+      return entity;
+    });
+  }, [world, learnedSkills]);
+};
+
+export const useLearnCycleSkill = (wandererEntity: Entity | undefined) => {
+  const { world, systems } = useMUD();
+
+  return useCallback(
+    async (skillEntity: Entity) => {
+      if (wandererEntity === undefined) throw new Error("No wanderer selected");
+      const tx = await systems["system.LearnCycleSkill"].executeTyped(
+        world.entities[wandererEntity],
+        world.entities[skillEntity],
+      );
+      await tx.wait();
+    },
+    [world, systems, wandererEntity],
+  );
+};
+
+export const usePermSkill = (wandererEntity: Entity | undefined) => {
+  const { world, systems } = useMUD();
+
+  return useCallback(
+    async (skillEntity: Entity) => {
+      if (wandererEntity === undefined) throw new Error("No wanderer selected");
+      const tx = await systems["system.PermSkill"].executeTyped(
+        world.entities[wandererEntity],
+        world.entities[skillEntity],
+      );
+      await tx.wait();
+    },
+    [world, systems, wandererEntity],
+  );
+};
+
+export const useExecuteNoncombatSkill = () => {
+  const { world, systems } = useMUD();
+
+  return useCallback(
+    async (cycleEntity: Entity, skillEntity: Entity) => {
+      const tx = await systems["system.NoncombatSkill"].executeTyped(
+        world.entities[cycleEntity],
+        world.entities[skillEntity],
+      );
+      await tx.wait();
+    },
+    [world, systems],
+  );
+};
