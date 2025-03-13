d22b5e233266a05c4406fb54b6a7fefe28c41618
dk1a <dk1a@protonmail.com>
2025-03-13 22:49:20 +0300

    embed PostDeploy into tests for local deployment for optimization

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.21;
 
 import { Script } from "forge-std/Script.sol";
+import { VmSafe } from "forge-std/Vm.sol";
 import { console } from "forge-std/console.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
@@ -17,27 +18,35 @@ import { LibInitEquipmentAffix } from "../src/namespaces/root/init/LibInitEquipm
 import { LibInitWheel } from "../src/namespaces/root/init/LibInitWheel.sol";
 import { LibInitERC721 } from "../src/namespaces/root/init/LibInitERC721.sol";
 
+// Init txs are large, especially affixes
+// Separating the script body allows it to be run directly within tests much faster, skipping lengthy broadcasts
+// In testnet/prod this would be a one-off long expensive deployment
+// TODO consider optimization where possible, some of it can be done offchain via ffi for example
+function runPostDeployInitializers(VmSafe vm, address worldAddress) {
+  // Specify a store so that you can use tables directly in PostDeploy
+  StoreSwitch.setStoreAddress(worldAddress);
+
+  // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+  uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+  // Start broadcasting transactions from the deployer account
+  vm.startBroadcast(deployerPrivateKey);
+
+  Idx_AffixPrototype_ExclusiveGroup.register();
+  UniqueIdx_AffixPrototype_TierName.register();
+
+  LibInitStatmod.init();
+  LibInitSkill.init();
+  LibInitGuise.init();
+  LibInitEquipmentAffix.init();
+  LibInitWheel.init();
+  LibInitERC721.init();
+
+  vm.stopBroadcast();
+}
+
 contract PostDeploy is Script {
   function run(address worldAddress) external {
-    // Specify a store so that you can use tables directly in PostDeploy
-    StoreSwitch.setStoreAddress(worldAddress);
-
-    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
-    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
-    // Start broadcasting transactions from the deployer account
-    vm.startBroadcast(deployerPrivateKey);
-
-    Idx_AffixPrototype_ExclusiveGroup.register();
-    UniqueIdx_AffixPrototype_TierName.register();
-
-    LibInitStatmod.init();
-    LibInitSkill.init();
-    LibInitGuise.init();
-    LibInitEquipmentAffix.init();
-    LibInitWheel.init();
-    LibInitERC721.init();
-
-    vm.stopBroadcast();
+    runPostDeployInitializers(vm, worldAddress);
   }
 }
